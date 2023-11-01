923d21dccb1a93f9cbd8cb2d2de62b35fcac340e
Ioana267 <ioana_coca@yahoo.com>
2023-11-01 15:24:04 +0200

    index screens

---
+++ screens/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home"
+import Articles  from "./Articles"
+import Settings from "./Settings"
+import Planet from "./Planet"
+
+export {
+    Home,
+    Articles,
+    Settings,
+    Planet,
+    
+}
