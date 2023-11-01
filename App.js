4503ceb6cec260ec1ded1e1886cefe3ca5c07271
Ioana267 <ioana_coca@yahoo.com>
2023-11-01 15:20:59 +0200

    Prima parte din bara

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, Platform,  View } from 'react-native';
+import { Home, Planet, Settings, Articles } from "./screens";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
+// Thanks for watching
+const Tab =createBottomTabNavigator();
+const screenOptions = {
+  tabBarShowLabel:false,
+  headerShown:false,
+  tabBarStyle:{
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 85,
+    backgroundColor: "#c4edc4"
+  }
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Nu</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+     <NavigationContainer>
+       <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                  <Entypo name="home" size={30} color={focused ? "#4F6F52": "#111"} />
+                  
+                </View>
+              )
+            }
+          }}
+          />
+          <Tab.Screen 
+          name="Planet" 
+          component={Planet} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <Entypo name="globe" size={30} color={focused ? "#4F6F52": "#111"} />
+                  
+                </View>
+              )
+            }
+          }}
+          />
+          <Tab.Screen 
+          name="Articles" 
+          component={Articles} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <Entypo name="book" size={30} color={focused ? "#4F6F52": "#111"} />
+                  
+                </View>
+              )
+            }
+          }}
+          />
+          
+          <Tab.Screen 
+          name="Settings" 
+          component={Settings} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <Ionicons name="settings" size={30}  color={focused ? "#4F6F52": "#111"} />
+                  
+                </View>
+              )
+            }
+          }}
+          />
+       </Tab.Navigator>
+     </NavigationContainer>
+)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
