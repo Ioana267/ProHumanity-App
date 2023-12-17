9c84755a4eace2c7339208490351738c765c65d6
Ioana267 <ioana_coca@yahoo.com>
2023-12-17 08:58:21 +0200

    Schimbari articole

--- screens/ArticleDetail.js
+++ screens/ArticleDetail.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+
+import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
 
 const ArticleDetail = ({ route }) => {
   const { article } = route.params;
-
   return (
-    <View>
-      <Image source={{ uri: article.image }} />
-      <Text>{article.title}</Text>
-      <Text>{article.content}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.bigDiv}>
+        <Image style={styles.articleImage} source={article.image} />
+        <Text style={styles.bigText}>{article.title}</Text>
+        
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  bigDiv: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#ecf9ec',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  articleDiv: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+  },
+  articleImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+  },
+  bigText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    // fontFamily: 'monteserrat',
+    backgroundColor: '#c4edc4',
+    width: 500,
+  },
+});
 
 export default ArticleDetail;
